b14ca9321c532590845405e784314382e875b73d
Jacob Boyce <jboyce313@gmail.com>
2023-05-19 08:45:16 -0500

    added mutations.js to client and edited header to link to login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,29 +2,28 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
-function Header({ display, setDisplay }) {
+function Header() {
   return (
     <header>
       <Container>
         <Row>
           <Col sm={12} lg={6}>
-            <h1 className="my-name">GameNights</h1>
+            <Link to="/">
+              <h1 className="my-name">GameNights</h1>
+            </Link>
           </Col>
           <Col className="nav-col" xs={12} sm={4} lg={2}>
-            <p className="nav-option" onClick={() => setDisplay("#")}>
-              My GameNights
-            </p>
+            <Link className="nav-option">My GameNights</Link>
           </Col>
           <Col className="nav-col" xs={12} sm={4} lg={2}>
-            <p className="nav-option" onClick={() => setDisplay("#")}>
+            <Link className="nav-option" to="/login">
               Log In
-            </p>
+            </Link>
           </Col>
           <Col className="nav-col" xs={12} sm={4} lg={2}>
-            <p className="nav-option" onClick={() => setDisplay("#")}>
-              Sign Up
-            </p>
+            <Link className="nav-option">Sign Up</Link>
           </Col>
         </Row>
       </Container>
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,9 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      _id
+    }
+  }
+`;
