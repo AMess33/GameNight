9275de66a9069fd05d95bc5f6a55a7b9b7158ab0
Jacob Boyce <jboyce313@gmail.com>
2023-05-19 14:59:41 -0500

    more css edits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
-    <div className="homepage">
-      <h2 className="homeSubHeader">
-        Welcome to <span className="siteName">GameNight</span>
-      </h2>
-      <p className="firstParagraph">
-        Your one-stop shop for tracking your party's ongoing game nights!
-      </p>
-      <p className="lastParagraph">
-        Create a game night, fill it with games, track scores, and more!
-      </p>
-      <Link className="getStartedBtn" to="/login">
-        Get Started
-      </Link>
+    <div className="homepage-container">
+      <div className="homepage">
+        <h2 className="homeSubHeader">
+          Welcome to <span className="siteName">GameNight</span>
+        </h2>
+        <p className="firstParagraph">
+          Your one-stop shop for tracking your party's ongoing game nights!
+        </p>
+        <p className="lastParagraph">
+          Create a game night, fill it with games, track scores, and more!
+        </p>
+        <Link className="getStartedBtn" to="/login">
+          Get Started
+        </Link>
+      </div>
     </div>
   );
 };
