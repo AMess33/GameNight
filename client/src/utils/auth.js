8aa2e56d906c8ccc465782d4c10a3ae9ee706b2b
Jacob Boyce <jboyce313@gmail.com>
2023-05-22 21:32:42 -0500

    added conditional rendering to header

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -21,7 +21,7 @@ class AuthService {
   }
 
   getToken() {
-    return localStorage.setItem("id_token");
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
@@ -31,7 +31,7 @@ class AuthService {
 
   logout() {
     localStorage.removeItem("id_token");
-    window.location.reload();
+    window.location.assign("/");
   }
 }
 
