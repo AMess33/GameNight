37c4094a1eaa942cebcda18d390d4e7e99cb4f86
Jacob Boyce <jboyce313@gmail.com>
2023-05-24 21:05:57 -0500

    gamenight buttons route to games, updated their style

--- client/src/components/GameNightList.js
+++ client/src/components/GameNightList.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 const GameNightList = ({ gamenights }) => {
-  const gameURL = `gamenight/${gamenights._id}`;
+  const gameURL = `/gamenight/${gamenights._id}`;
   return (
     <div className="card game">
       <div className="card-body">
         <h3 className="card-title">{gamenights.title}</h3>
         <p className="card-text">{gamenights.description}</p>
         <p className="card-text">{gamenights.games.length} games started.</p>
-        <a href={gameURL} className="btn btn-primary">
+        <Link className="btn btn-outline-light" to={gameURL}>
           Open Game Night!
-        </a>
+        </Link>
+        {/* <a>Open Game Night</a> */}
       </div>
     </div>
   );
