2e89f732a7e12a7ec0718f02bbe706363221b759
Jacob Boyce <jboyce313@gmail.com>
2023-05-25 09:32:44 -0500

    tried to add modal to removegameform

--- client/src/components/SingleGameNight/RemoveGameForm.js
+++ client/src/components/SingleGameNight/RemoveGameForm.js
@@ -1,36 +1,38 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
-import { useMutation } from '@apollo/client';
-import { REMOVE_GAME } from '../../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { REMOVE_GAME } from "../../utils/mutations";
+import { Alert } from "@mui/material";
 
 const RemoveGameForm = ({ game }) => {
-  
   const [removeGame] = useMutation(REMOVE_GAME, {
     // TODO: Use modal instead of alerts
-    onCompleted: () => { window.alert(`${game.name} has been removed from the database.`) },
-    onError: () => { alert('Something went wrong') }
+    onCompleted: () => {
+      window.alert(`${game.name} has been removed from the database.`);
+    },
+    onError: () => {
+      alert("Something went wrong");
+    },
   });
 
   const handleClick = (e) => {
     try {
-      removeGame({ variables: { gameId: game._id } })
+      removeGame({ variables: { gameId: game._id } });
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
-    <div className="float-end">
-      <Button 
-        variant="outline-light" 
-        size="sm"
-        onClick={handleClick}
-      >
-        Remove
-      </Button>
+    <div>
+      <div className="float-end">
+        <Button variant="outline-light" size="sm" onClick={handleClick}>
+          Remove
+        </Button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default RemoveGameForm;
