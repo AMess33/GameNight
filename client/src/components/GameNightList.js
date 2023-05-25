557dd61de3eb121642354ee32a065e937bf4276d
Jacob Boyce <jboyce313@gmail.com>
2023-05-24 20:54:57 -0500

    route from gamenight to games, add description to gamenight

--- client/src/components/GameNightList.js
+++ client/src/components/GameNightList.js
@@ -1,15 +1,16 @@
 const GameNightList = ({ gamenights }) => {
+  const gameURL = `gamenight/${gamenights._id}`;
   return (
-      <div className="card game">
-        <div className="card-body">
-          <h3 className="card-title">{gamenights.title}</h3>
-          <p className="card-text">{gamenights.description}</p>
-          <p className="card-text">{gamenights.games.length} games started.</p>
-          <a href="#" className="btn btn-primary">
-            Open Game Night!
-          </a>
-        </div>
+    <div className="card game">
+      <div className="card-body">
+        <h3 className="card-title">{gamenights.title}</h3>
+        <p className="card-text">{gamenights.description}</p>
+        <p className="card-text">{gamenights.games.length} games started.</p>
+        <a href={gameURL} className="btn btn-primary">
+          Open Game Night!
+        </a>
       </div>
-  );  
+    </div>
+  );
 };
 export default GameNightList;
--- client/src/pages/GameNight.js
+++ client/src/pages/GameNight.js
@@ -4,17 +4,16 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 
 import "google-fonts";
 
 import Widgets from "../components/Widgets";
-import { Game, AddGameForm } from '../components/SingleGameNight';
-import { QUERY_GAME_NIGHT } from '../utils/queries';
+import { Game, AddGameForm } from "../components/SingleGameNight";
+import { QUERY_GAME_NIGHT } from "../utils/queries";
 
 const GameNight = (props) => {
-  
   const { gameNightId } = useParams();
 
   const { loading, error, data } = useQuery(QUERY_GAME_NIGHT, {
@@ -22,24 +21,30 @@ const GameNight = (props) => {
   });
 
   if (loading) {
-    return "Loading..."
+    return "Loading...";
   }
   if (error) {
-    return `Error! ${error.message}`
+    return `Error! ${error.message}`;
   }
 
   return (
     <Container>
       <Row>
-        {data && ( 
+        {data && (
           <>
             <Col className="gameSide" lg={10}>
-              <h2 className="gameNightName">{data.gameNight && data.gameNight.title}</h2>
+              <h2 className="gameNightName">
+                {data.gameNight && data.gameNight.title}
+              </h2>
+              <p>{data.gameNight && data.gameNight.description}</p>
               <div className="gamesList">
-                <AddGameForm gameNightId={data.gameNight && data.gameNight._id } />
-                {data.gameNight && data.gameNight.games.map((game) => (
-                  <Game key={game._id} game={game} />
-                ))}
+                <AddGameForm
+                  gameNightId={data.gameNight && data.gameNight._id}
+                />
+                {data.gameNight &&
+                  data.gameNight.games.map((game) => (
+                    <Game key={game._id} game={game} />
+                  ))}
               </div>
             </Col>
             <Col lg={2}>
